7438fc126ce5e8018182534f2f92a3055fd67b81
RahulChandrakarr <rrahulchandrkar1@gmail.com>
2026-01-03 12:26:04 +0530

    with changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import PublicDashboard from '@/components/PublicDashboard';
 export default function Home() {
   return (
     <div className="flex-1 w-full flex flex-col">
-      {/* Dashboard */}
+      {/* Dashboard  page with the changes*/}
       <div className="container mx-auto px-4 py-10">
         <PublicDashboard />
       </div>
